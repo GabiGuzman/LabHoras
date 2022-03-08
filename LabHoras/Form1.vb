69442a6a0069a36bd94f10b2f32138bdcbca3e39
Guzman <Guzman@DESKTOP-29GVUO5>
2022-03-08 10:32:16 -0600

    Agregar archivos de proyecto.

---
+++ LabHoras/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As Double
+        a = TextBox1.Text
+
+        If (a <= 12) Then
+            TextBox2.Text = "AM"
+        End If
+        If (a >= 13) Then
+            TextBox2.Text = "PM"
+        End If
+    End Sub
+End Class
